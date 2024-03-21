840f34070b0eb9433bcba2841ea4b6ea622f6a7a
devxshubham <devshubhamcode@gmail.com>
2024-03-21 21:04:55 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+export const Navbar = ()=>{
+
+    return <div className="flex p-5 items-center justify-between flex-grow border-b-[2px] gap-1">
+        <div className="flex justify-betwee gap-5 flex-grow">
+            <div className="text-2xl font-bold">
+                Exclusive
+            </div>
+            <div className="flex items-center gap-5 ">
+                <button className="text-[16px] font-medium">Home</button>
+                <button className="text-[16px] font-medium">contact</button>
+                <button className="text-[16px] font-medium">About</button>
+                <button className="text-[16px] font-medium">Sign Up</button>
+            </div>
+        </div>
+        <div className="flex items-center gap-2">
+            <div className="flex items-center gap-1 bg-[#F5F5F5] py-[7px] rounded px-[15px]">
+                <input className="bg-[#F5F5F5]" placeholder="What are you looink for ?" type="text" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                </svg>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </svg>
+            </div>
+            <div className="bg-[#DB4444] text-white p-2 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                </svg>
+            </div>
+        </div>
+    </div>
+}
