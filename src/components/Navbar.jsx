7821a6818f489147c90adabef746dc81629605c9
devxshubham <devshubhamcode@gmail.com>
2024-03-31 23:58:14 +0530

    fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export const Navbar = ()=>{
                 }}>Sign Up</button>
             </div>
         </div>
-        <div className="flex items-center gap-4 md:gap-4">
+        <div className="flex items-center cursor-pointer gap-4 md:gap-4">
             <div className="flex items-center gap-1 bg-[#F5F5F5] py-[7px] rounded px-[15px]">
                 <input className="bg-[#F5F5F5] hidden md:block" placeholder="What are you looink for ?" type="text" />
                 <BsSearch size={20} />
@@ -38,11 +38,14 @@ export const Navbar = ()=>{
             <div>
                 <AiOutlineHeart size={30} />
             </div>
-            <div className="relative p-2">
-                {(kart>0) ? <div className="flex justify-center items-center absolute top-0 right-0 bg-[#DB4444] rounded-full size-5 text-white">{kart}</div> : <></>}
+            <div className="relative cursor-pointer p-2" onClick={()=>{
+                navigate('/product/kart')
+            }}>
+                {(kart>0) ? <div className="flex justify-center  items-center absolute top-0 right-0 bg-[#DB4444] rounded-full size-5 text-white">{kart}</div> : <></>}
                 <AiOutlineShoppingCart size={30} />
+
             </div>
-            <div className="bg-[#DB4444] text-white p-2 rounded-full">
+            <div className="cursor-pointer bg-[#DB4444] text-white p-2 rounded-full">
                 <AiOutlineUser size={25} />
             </div>
         </div>
